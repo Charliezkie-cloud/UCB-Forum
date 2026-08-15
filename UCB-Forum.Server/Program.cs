0f6c6f731c568b404d0e19be74bad2b3774b39d1
Charles Henry Tinoy <charleshenrytinoy8@gmail.com>
2026-08-15 15:36:32 +0800

    Added auto database update for production.

--- UCB-Forum.Server/Program.cs
+++ UCB-Forum.Server/Program.cs
@@ -81,6 +81,13 @@ builder.Services.AddForumRateLimiting(builder.Configuration);
 
 var app = builder.Build();
 
+if (app.Environment.IsProduction())
+{
+    using var scope = app.Services.CreateScope();
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    await dbContext.Database.MigrateAsync();
+}
+
 // Configure the HTTP request pipeline.
 app.UseExceptionHandler();
 
